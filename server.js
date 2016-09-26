ddba8c7df43fad7f0600014a6708f23ffeff1a46
Rangy Abines <rangyabines11@gmail.com>
2016-09-25 20:52:58 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static('./public'));
 // -------------------------------------------------
 
 // MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect('mongodb://localhost/nytreact');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds041496.mlab.com:41496/heroku_566sh6mn');
 var db = mongoose.connection;
 
 db.on('error', function (err) {
